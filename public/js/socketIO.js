ffcfe38724054c4a30fbb12cd3982f54490d63b0
Xchel_Sanchez <xchelsvz@gmail.com>
2015-11-10 10:57:41 -0600

    Adding Sound Notifications

--- public/js/socketIO.js
+++ public/js/socketIO.js
@@ -3,17 +3,32 @@ $(document).ready(function(){
 	var socket = io();
 	$('form').submit(function(){
 
-		socket.emit('BusAlert',$('#location').val());
+		var date = new Date();
+		var time = date.getHours() +' : '+date.getMinutes();
+		var msg = { location: $('#location').val(), unidad: 'Unidad 10-SD', time:  time }
+
+		socket.emit('BusAlert', msg);
 		$('#location').val('');
 
 		$('#okDiv').show('slow')
+
+		window.setTimeout(hide, 2200);
+
+		function hide (){
+			$('#okDiv').hide('slow')
+		}
 
 		return false;
 
 	})
 
 	socket.on('BusAlert', function (msg){
-		$('#AlertList').append($('<a class="list-group-item">'+msg+'<button class="btn btn-primary">Asistir Alerta</button></a>'));
+		
+		var audio = new Audio('../sounds-928-gentle-alarm.mp3');
+		audio.play();
+
+		$('#AlertList').append($('<a class="list-group-item" href="#" align="center">Alerta de: <strong>'+msg.unidad+'</strong> con ubicación cercana a la estación: <strong>'+msg.location+'</strong> a las: <strong>'+msg.time+'</strong> horas</a>'));
+
 	})
 
 })
